3ae5f8a49d39cfcc417bae96d25376c81db5cd49
Oleg Suvorov <oleg.suvorov@omnigon.com>
2016-04-12 13:59:23 +0300

    Clean "our team"

--- js/script.js
+++ js/script.js
@@ -1,27 +1,11 @@
-// making square
-function make_sqrt () {
-	// making photo
-		var photo = document.getElementById('oleg');
-		var computedStylePhoto = getComputedStyle(photo);
-		var newHight = parseInt(computedStylePhoto.width)*1.3333;
-		var photoArr = document.getElementsByClassName('photo');
-		for(var i = 0 ; i<photoArr.length; i++){
-			photoArr[i].style.height = newHight+"px";
-		}
-};
-
-$(document).ready = make_sqrt();
-
-
- $('.carousel').carousel();
-// skroll top
-$('#upArr').click(function skroll () {
-	 $('html, body').animate({scrollTop: 0},1500);
-})
+// scroll top
+$('#upArr').click(function scroll() {
+    $('html, body').animate({scrollTop: 0}, 1500);
+});
 
 $('#open-menu').click(function () {
-	$('#uncol-menu').slideToggle();
+    $('#uncol-menu').slideToggle();
 });
 $('#close-menu').click(function () {
-	$('#uncol-menu').slideToggle();
+    $('#uncol-menu').slideToggle();
 });
