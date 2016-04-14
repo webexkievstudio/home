15e478f51f47309b912653b4e0cd8674b1280609
Oleg Suvorov <oleg.suvorov@omnigon.com>
2016-04-14 15:48:23 +0300

    clean shit, update js

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,34 +1,14 @@
- //making square
-function make_sqrt () {
-	// making photo
-		var photo = document.getElementById('oleg');
-		var computedStylePhoto = getComputedStyle(photo);
-		var newHight = parseInt(computedStylePhoto.width)*1.3333;
-		var photoArr = document.getElementsByClassName('photo');
-		for(var i = 0 ; i<photoArr.length; i++){
-			photoArr[i].style.height = newHight+"px";
-		}
-};
+$(document).ready(function () {
 
-$(document).ready = make_sqrt();
-$(document).ready(function(){
+    $('#upArr').click(function scroll() {
+        $('html, body').animate({scrollTop: 0}, 1500);
+    });
 
- $('.carousel').carousel();
-// skroll top
-$('#upArr').click(function skroll () {
-	 $('html, body').animate({scrollTop: 0},1500);
-})
+    $('#open-menu, #close-menu').click(function () {
+        $('#uncol-menu').slideToggle();
+    });
 
-$('#open-menu').click(function () {
-	$('#uncol-menu').slideToggle();
-});
-$('#close-menu').click(function () {
-	$('#uncol-menu').slideToggle();
-});
-    
-    
-    
-     var mySwiper = new Swiper('.swiper-container', {
+    var mySwiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
         nextButton: '.swiper-button-next',
